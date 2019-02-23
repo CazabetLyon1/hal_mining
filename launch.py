94afa9b303bcbe2754182724ae9d6e41cfdb8c11
VIncent <vincent.toinon@outlook.fr>
2019-02-23 15:20:53 +0100

    derniere version du remplissage de la bd cette fois sans doublon

--- launch.py
+++ launch.py
@@ -1,7 +1,8 @@
 import requests
 import json
-from pymongo import MongoClient
 import urllib
+from pymongo import MongoClient
+
 
 ##MongoDB  Database creation 
 client = MongoClient("mongodb://localhost:27017/")
@@ -12,33 +13,43 @@ db = client.DATABASETEST
 dblist = client.list_database_names()
 print(dblist)
 if "DATABASETEST" in dblist:
-  print("Database creation successfull." )
+    print("Database creation successfull." )
 
 col = db["articles"]
 collist = db.list_collection_names()
 if "articles" in collist:
-  print("The collection exists.")
-
+    print("The collection exists.")
 ## init requete
-filtres ="&fl=title_s, docType_s, authFullName_s, ePublicationDate_tdate, domain_s, domainAllCode_s, structName_s, languages_s"
+filtres ='&fl=docid, title_s,docType_s,authFullName_s, ePublicationDate_tdate, domain_s, domainAllCode_s, structName_s, languages_s'
+
 cursor= "*"
+
 url='https://api.archives-ouvertes.fr/search/?q=city_s:Lyon'+filtres+'&rows=10000&sort=docid%20asc&cursorMark='+cursor
 
 r = requests.request('GET',url)
 jsons=json.loads(r.text)
 
+
 while cursor != jsons['nextCursorMark']:
   
-    for item in jsons['response']['docs']:
-        #database add
-        col.insert_one(item)
-    #2nd request init
-    str2 = urllib.parse.quote(jsons['nextCursorMark'])
-    url= url.replace(cursor,str2)
-    cursor = str2
-
-    r=requests.request('GET', url) 
-    jsons=json.loads(r.text)
-  
+   for item in jsons['response']['docs']:
        
+       #database add    
+       if col.count_documents({"docid": item['docid']}) == 0:
+           col.insert(item)
+           print("insert success")
+
+       else:
+           print('Ce document a déjà été ajouté')                   
+  
+   #2nd request init
+   tmpCurs = urllib.parse.quote(jsons['nextCursorMark'])
+   url= url.replace(cursor,tmpCurs)
+   cursor = tmpCurs
+   r=requests.request('GET', url) 
+   jsons=json.loads(r.text)
+
+
 client.close()
+
+
