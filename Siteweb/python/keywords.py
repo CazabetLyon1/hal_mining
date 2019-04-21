b3bcf0c624f5f7e4cd9e8619f11542755dc93d04
VIncent <vincent.toinon@outlook.fr>
2019-04-21 23:56:37 +0200

    passage de plusieurs parametres possible

--- Siteweb/python/keywords.py
+++ Siteweb/python/keywords.py
@@ -3,16 +3,21 @@ import json
 
 def decoupeTitre(titre , liste, countKW):
     titre.replace('\'', ' ')
+    titre.replace(',', ' ')
     listeTitre = str.split(' ')
+    print(listeTitre[0])
     for x in listeTitre:
+        print(x)
         if  len(x)>3:
             if x in liste:
                 countKW[liste.index(x)] += 1
             else:
-                liste.append(x)
-                countKW.append(1)
-                print(len(liste))
-    
+                try:
+                    liste.append(x)
+                    countKW.append(1)
+                    print(len(liste))
+                except IndexError:
+                    print(IndexError)
    
             
 def KWClassement():
@@ -20,12 +25,13 @@ def KWClassement():
     i=0
     max = 0
     idMax=0
-    print(len(countKW))
     while i < 10:
         for it in countKW:
-            if it>max:
-                max=it
-                idMax = countKW.index(it)
+            print(it)
+            if countKW[it]>max:
+                max=countKW[it]
+                print(max)
+                idMax = it
         dico[str(i)] = listeKW[idMax]
         i+=1
     return dico
@@ -41,12 +47,11 @@ countKW=[]
 
 for x in col.find({"labStructName_s": {'$exists': 1},  "labStructAddress_s": {'$exists': 1}}):
     for y in x['title_s']:
-        print(x['labStructAddress_s'])
+        
         s=str(x['labStructAddress_s'])
-        print((s.find('Lyon'))!=-1 or (s.find('Villeurbanne'))!=-1)
-        if(('Lyon' or 'Villeurbanne')in s):
-            strin =y
-            decoupeTitre(strin, listeKW, countKW)
+        if(('Lyon' or 'Villeurbanne' or "BRON")in s):
+           
+            decoupeTitre(y, listeKW, countKW)
     
 
 d= KWClassement()
